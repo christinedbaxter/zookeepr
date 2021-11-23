983c3789b0247dba5d1202806d0737f665c14571
Christine Baxter <christine.d.baxter@live.com>
2021-11-22 19:43:13 -0600

    add findById get method

--- server.js
+++ server.js
@@ -42,6 +42,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get("/api/animals", (req, res) => {
     let results = animals;
     if (req.query) {
@@ -49,6 +54,15 @@ app.get("/api/animals", (req, res) => {
     }
     res.json(results);
 })
+
+app.get("/api/animals/:id", (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
