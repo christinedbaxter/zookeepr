386b2ebadd412cb5d22478681fc10b24b4798fde
Christine Baxter <christine.d.baxter@live.com>
2021-11-23 18:33:30 -0600

    accept, validate, and write user-created data

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 const { animals } = require("./data/animals");
 
 function filterByQuery(query, animalsArray) {
@@ -47,6 +54,32 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, "./data/animals.json"),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== "string") {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== "string") {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== "string") {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
 app.get("/api/animals", (req, res) => {
     let results = animals;
     if (req.query) {
@@ -61,6 +94,20 @@ app.get("/api/animals/:id", (req, res) => {
         res.json(result);
     } else {
         res.sendStatus(404);
+    }
+});
+
+app.post("/api/animals", (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send("The animal is not properly formatted.");
+    } else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
     }
 });
 
